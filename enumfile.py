ef4cfe9ffa2ef7bbc57539273516d3527a6f81c3
Sheldon_xia <xiaxia47@163.com>
2017-10-30 11:48:15 +0800

    Enum枚举类的使用

---
+++ enumfile.py
@@ -0,0 +1,8 @@
+# -*- coding:gbk
+from enum import Enum
+
+Month = Enum('Month',('Jan','Feb','Mar','Apr','May','Jun',
+                      'Jul','Aug','Sep','Oct','Nov','Dec'))
+
+for name,member in Month.__members__.items():
+    print(name,'=>', member, ',',member.value)
