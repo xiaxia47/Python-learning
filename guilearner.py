2709ad2c894973423ec7892bc6ee53fbea1c8dfb
Sheldon_xia <xiaxia47@163.com>
2017-10-30 11:53:10 +0800

    简单的GUI开发

---
+++ guilearner.py
@@ -0,0 +1,31 @@
+#-*- coding=gbk
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self,text= 'Hello', command = self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message','Hello, %s' % name)
+
+'''
+        self.helloLabel = Label(self, text='Hello,world')
+        self.helloLabel.pack()
+        self.quitButton = Button(self,text='Quit',command = self.quit)
+        self.quitButton.pack()
+'''
+
+app = Application()
+app.master.title('Hello World')
+app.mainloop()
+
