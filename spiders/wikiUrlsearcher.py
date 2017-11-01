293969c7b67392dcd9fb66e99fb35bddc0076b03
Sheldon_xia <xiaxia47@163.com>
2017-11-01 16:38:50 +0800

    递归遍历维基百科的网页目录

---
+++ spiders/wikiUrlsearcher.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html,"html.parser")
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
+        if 'href' in link.attrs and link.attrs['href'] not in pages:
+            #我们遇到了新页面
+            newPage = link.attrs['href']
+            print(newPage)
+            pages.add(newPage)
+            getLinks(newPage)
+getLinks("")
