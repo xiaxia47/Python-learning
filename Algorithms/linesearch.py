d8598b386b1ed25e1e19a56fa3550c34eea5b26a
Sheldon_xia <xiaxia47@163.com>
2017-11-01 09:54:30 +0800

    无序列表查找 时间复杂度 O(n)

--- Algorithms/linesearch.py
+++ Algorithms/linesearch.py
@@ -1,6 +1,11 @@
 #/usr/bin/env python
 #-*- coding=utf-8
 
+#也就是数据不排序的线性查找，遍历数据元素。
+#算法分析：最好情况是在第一个位置就找到了，此为O(1)；最坏情况在最后一个位置才找到，此为O(n)；所以平均查找次数为(n+1)/2。
+#时间复杂度为:O(n)
+
+
 def sequential_search(lis key):
     length = len(lis)
     for i in range(length):
